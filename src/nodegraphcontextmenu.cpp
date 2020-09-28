df0ffcf9b22c6b7cf5e1a5446bdfede54e80535e
till <till.dechent@gmail.com>
2020-09-28 16:49:18 +0100

    Rearranged context menu.

--- src/nodegraphcontextmenu.cpp
+++ src/nodegraphcontextmenu.cpp
@@ -10,19 +10,38 @@ using namespace Cascade;
 NodeGraphContextMenu::NodeGraphContextMenu(NodeGraph* parent)
     : QMenu(parent)
 {
+    this->setFixedWidth(150);
+
+    // Add Read Node at the top
+    auto a = new QAction();
+    actions.push_back(a);
+    a->setText("Read");
+    this->addAction(a);
+    auto t = NODE_TYPE_READ;
+    QObject::connect(
+                a,
+                &QAction::triggered,
+                parent,
+                [parent, t]{ parent->createNode(t); });
+
+    this->addSeparator();
+
     QMapIterator<NodeType, QString> i(nodeStrings);
     while (i.hasNext())
     {
         i.next();
-        auto a = new QAction();
-        actions.push_back(a);
-        a->setText(i.value());
-        this->addAction(a);
-        auto t = i.key();
-        QObject::connect(
-                    a,
-                    &QAction::triggered,
-                    parent,
-                    [parent, t]{ parent->createNode(t); });
+        if (i.key() != NODE_TYPE_READ)
+        {
+            auto a = new QAction();
+            actions.push_back(a);
+            a->setText(i.value());
+            this->addAction(a);
+            auto t = i.key();
+            QObject::connect(
+                        a,
+                        &QAction::triggered,
+                        parent,
+                        [parent, t]{ parent->createNode(t); });
+        }
     }
 }
