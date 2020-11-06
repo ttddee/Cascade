d222b6a7d130c77cab8c933bf586b01220615f2a
gituser <till.dechent@gmail.com>
2020-11-06 19:02:22 +0000

    Changed includes to relative paths.

--- src/vulkanview.cpp
+++ src/vulkanview.cpp
@@ -38,7 +38,8 @@ VulkanView::VulkanView(ViewerStatusBar* statusBar, QWidget *parent)
                        << "VK_LAYER_LUNARG_core_validation"
                        << "VK_LAYER_LUNARG_image"
                        << "VK_LAYER_LUNARG_swapchain"
-                       << "VK_LAYER_GOOGLE_unique_objects");
+                       << "VK_LAYER_GOOGLE_unique_objects"
+                       << "VK_LAYER_KHRONOS_validation");
 
     if (!instance.create())
     {
