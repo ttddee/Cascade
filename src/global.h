e7f14c19ecddb742eea6f41e57fbdb03af849681
Yaraslau <44506630+Yaraslaut@users.noreply.github.com>
2022-05-14 22:05:10 +0300

    Update global.h

--- src/global.h
+++ src/global.h
@@ -35,14 +35,14 @@ static const QMap<ViewerMode, QString> sViewerModeText =
     {
         [[maybe_unused]] static constexpr ViewerMode sDefaultViewerMode = ViewerMode::eOutputRgb;
 
-        [[maybe_unused]] static constexpr QColor sRed(229, 70, 61);
-        [[maybe_unused]] static constexpr QColor sGreen(53, 226, 87);
-        [[maybe_unused]] static constexpr QColor sBlue(35, 114, 239);
+        [[maybe_unused]] static QColor sRed(229, 70, 61);
+        [[maybe_unused]] static QColor sGreen(53, 226, 87);
+        [[maybe_unused]] static QColor sBlue(35, 114, 239);
 
         // Node
         [[maybe_unused]] static constexpr int sNodeCornerRadius(6);
-        [[maybe_unused]] static constexpr QColor sDefaultNodeColor(24, 27, 30);
-        [[maybe_unused]] static constexpr QColor sSelectedNodeColor(37, 74, 115);
+        [[maybe_unused]] static QColor sDefaultNodeColor(24, 27, 30);
+        [[maybe_unused]] static QColor sSelectedNodeColor(37, 74, 115);
 
     } // namespace Cascade::Config
 
